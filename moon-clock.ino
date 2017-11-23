c2b4699fcf665d598fcaca804e74d7106ebfc050
Samuel Quintana <sock.sqt@gmail.com>
2017-11-22 22:14:59 -0600

    Integrated 74hc595 and 7 segments displays, pending moon phases

--- moon-clock.ino
+++ moon-clock.ino
@@ -11,18 +11,63 @@
 #define MOON_PIN_6        8
 #define MOON_PIN_7        9
 
-const short TOTAL_PHASES = 8;
+// Digits controll
+#define CTRL_DIGIT_ONE    10
+#define CTRL_DIGIT_TWO    11
+#define CTRL_DIGIT_THREE  12
+#define CTRL_DIGIT_FOUR   13
+#define CTRL_DOTS         A0
+
+//
+#define SHIFT_REGISTER_DATA     A1
+#define SHIFT_REGISTER_LATCH    A2
+#define SHIFT_REGISTER_CLOCK    A3
+
+// Configuration pins
+#define CTRL_24_HRS_MODE   A6
+#define _24_HRS_MODE       A7
+
+// digit pattern for a 7-segment display
+const byte seven_segments_config[16] = {
+// .GFEDCBA
+  B11000000,  // 0
+  B11111001,  // 1
+  B10100100,  // 2
+  B10110000,  // 3
+  B10011001,  // 4
+  B10010010,  // 5
+  B10000010,  // 6
+  B11111000,  // 7
+  B10000000,  // 8
+  B10010000,  // 9
+  B10001000,  // A
+  B10000011,  // b
+  B11000110,  // C
+  B10100001,  // d
+  B10000110,  // E
+  B10001110   // F
+};
+
+const int digit_controllers[] = {
+  CTRL_DIGIT_ONE, CTRL_DIGIT_TWO, CTRL_DIGIT_THREE, CTRL_DIGIT_FOUR
+};
+int BASE_DELAY = 2;
+const int DOTS_TOGGLE = 1000;
 
 RtcDS3231<TwoWire> Rtc(Wire);
+int lastDay = 0;
+int lastMonth = 0;
+int lastYear = 0;
+int dotCounter = 0;
 
 void setup(void) {
-  Serial.begin(57600);
+  //Serial.begin(57600);
   Rtc.Begin();
 
-  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
-  //RtcDateTime compiled = RtcDateTime(2017, 12, 4, 23, 22, 0);
+  //RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  //RtcDateTime compiled = RtcDateTime(2017, 12, 4, 6, 52, 0);
 
-  Rtc.SetDateTime(compiled);
+  //Rtc.SetDateTime(compiled);
 
   pinMode(MOON_PIN_0, OUTPUT);
   pinMode(MOON_PIN_1, OUTPUT);
@@ -32,16 +77,116 @@ void setup(void) {
   pinMode(MOON_PIN_5, OUTPUT);
   pinMode(MOON_PIN_6, OUTPUT);
   pinMode(MOON_PIN_7, OUTPUT);
+
+  pinMode(CTRL_DIGIT_ONE, OUTPUT);
+  pinMode(CTRL_DIGIT_TWO, OUTPUT);
+  pinMode(CTRL_DIGIT_THREE, OUTPUT);
+  pinMode(CTRL_DIGIT_FOUR, OUTPUT);
+  pinMode(CTRL_DOTS, OUTPUT);
+
+  pinMode(CTRL_24_HRS_MODE, INPUT);
+  pinMode(_24_HRS_MODE, OUTPUT);
+
+  pinMode(SHIFT_REGISTER_DATA, OUTPUT);
+  pinMode(SHIFT_REGISTER_LATCH, OUTPUT);
+  pinMode(SHIFT_REGISTER_CLOCK, OUTPUT);
 }
 
 void loop(void) {
   RtcDateTime now = Rtc.GetDateTime();
-  int phase = getMoonPhase(now.Day(), now.Month(), now.Year());
+  int currentDay = now.Day();
+  int currentMonth = now.Month();
+  int currentYear = now.Year();
 
-  printPhase(phase);
+  if (lastDay == 0 & lastMonth == 0 && lastYear == 0) {
+    lastDay = currentDay;
+    lastMonth = currentMonth;
+    lastYear = currentYear;
+
+    printPhase(currentDay, currentMonth, currentYear);
+  } else if (lastDay != currentDay || lastMonth != currentMonth || lastYear != currentYear) {
+    printPhase(currentDay, currentMonth, currentYear);
+  }
 
   //printDateTime(now);
-  delay(10000); // ten seconds
+  printSegments(now.Hour(), now.Minute());
+}
+
+void printSegments(int hour, int minutes) {
+  int hours_mode = analogRead(CTRL_24_HRS_MODE);
+
+  if (hours_mode >= 100) {
+    if (hour > 12) {
+      digitalWrite(_24_HRS_MODE, 255);
+      hour -= 12;
+    } else {
+      digitalWrite(_24_HRS_MODE, 0);
+    }
+  }
+  
+  int hour_parts[] = {
+    minutes % 10,
+    minutes / 10,
+    hour % 10,
+    hour / 10
+  };
+
+  // get the digit pattern to be updated
+  byte pattern;
+  pattern = seven_segments_config[hour_parts[0]];
+  digitalWrite(SHIFT_REGISTER_LATCH, LOW);
+  shiftOut(SHIFT_REGISTER_DATA, SHIFT_REGISTER_CLOCK, MSBFIRST, ~pattern);
+  digitalWrite(SHIFT_REGISTER_LATCH, HIGH);
+
+  digitalWrite(digit_controllers[0], LOW);
+  digitalWrite(digit_controllers[1], HIGH);
+  digitalWrite(digit_controllers[2], HIGH);
+  digitalWrite(digit_controllers[3], HIGH);
+  delay(BASE_DELAY); // ten 100ms
+
+  // get the digit pattern to be updated
+  pattern = seven_segments_config[hour_parts[1]];
+  digitalWrite(SHIFT_REGISTER_LATCH, LOW);
+  shiftOut(SHIFT_REGISTER_DATA, SHIFT_REGISTER_CLOCK, MSBFIRST, ~pattern);
+  digitalWrite(SHIFT_REGISTER_LATCH, HIGH);
+
+  digitalWrite(digit_controllers[0], HIGH);
+  digitalWrite(digit_controllers[1], LOW);
+  digitalWrite(digit_controllers[2], HIGH);
+  digitalWrite(digit_controllers[3], HIGH);
+  delay(BASE_DELAY); // ten 100ms
+
+  // get the digit pattern to be updated
+  pattern = seven_segments_config[hour_parts[2]];
+  digitalWrite(SHIFT_REGISTER_LATCH, LOW);
+  shiftOut(SHIFT_REGISTER_DATA, SHIFT_REGISTER_CLOCK, MSBFIRST, ~pattern);
+  digitalWrite(SHIFT_REGISTER_LATCH, HIGH);
+
+  digitalWrite(digit_controllers[0], HIGH);
+  digitalWrite(digit_controllers[1], HIGH);
+  digitalWrite(digit_controllers[2], LOW);
+  digitalWrite(digit_controllers[3], HIGH);
+  delay(BASE_DELAY); // ten 100ms
+
+  if (hour >= 10) {
+    // get the digit pattern to be updated
+    pattern = seven_segments_config[hour_parts[3]];
+    digitalWrite(SHIFT_REGISTER_LATCH, LOW);
+    shiftOut(SHIFT_REGISTER_DATA, SHIFT_REGISTER_CLOCK, MSBFIRST, ~pattern);
+    digitalWrite(SHIFT_REGISTER_LATCH, HIGH);
+  
+    digitalWrite(digit_controllers[0], HIGH);
+    digitalWrite(digit_controllers[1], HIGH);
+    digitalWrite(digit_controllers[2], HIGH);
+    digitalWrite(digit_controllers[3], LOW);
+    delay(BASE_DELAY); // ten 100ms
+  }
+
+  dotCounter += (BASE_DELAY * 4);
+  if (dotCounter >= DOTS_TOGGLE) {
+    digitalWrite(CTRL_DOTS, !digitalRead(CTRL_DOTS));
+    dotCounter = 0;
+  }
 }
 
 int getMoonPhase(int currDay, int currMonth, int currYear) {
@@ -60,7 +205,9 @@ int getMoonPhase(int currDay, int currMonth, int currYear) {
   return r < 0 ? r + 30 : r;
 }
 
-void printPhase(int moonPhase) {
+void printPhase(int currDay, int currMonth, int currYear) {
+  int moonPhase = getMoonPhase(currDay, currMonth, currYear);
+  
   if (moonPhase == 0) {
     digitalWrite(MOON_PIN_0, LOW);
     digitalWrite(MOON_PIN_1, LOW);
@@ -134,23 +281,5 @@ void printPhase(int moonPhase) {
     digitalWrite(MOON_PIN_6, HIGH);
     digitalWrite(MOON_PIN_7, HIGH);
   }
-}
-
-#define countof(a) (sizeof(a) / sizeof(a[0]))
-
-void printDateTime(const RtcDateTime& dt) {
-  char datestring[20];
-
-  snprintf_P(datestring, 
-          countof(datestring),
-          PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
-          dt.Month(),
-          dt.Day(),
-          dt.Year(),
-          dt.Hour(),
-          dt.Minute(),
-          dt.Second() );
-  Serial.print(datestring);
-  Serial.print('\n');
 }
 
